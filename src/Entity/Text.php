d4552c1bc5ac055d24d805321abd4006eaadb6ec
hugo-mtn7 <hugo.mtn7@gmail.com>
2018-11-20 11:45:50 +0100

    entity text

---
+++ src/Entity/Text.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TextRepository")
+ */
+class Text
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $fr;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $en;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $es;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFr(): ?string
+    {
+        return $this->fr;
+    }
+
+    public function setFr(string $fr): self
+    {
+        $this->fr = $fr;
+
+        return $this;
+    }
+
+    public function getEn(): ?string
+    {
+        return $this->en;
+    }
+
+    public function setEn(string $en): self
+    {
+        $this->en = $en;
+
+        return $this;
+    }
+
+    public function getEs(): ?string
+    {
+        return $this->es;
+    }
+
+    public function setEs(string $es): self
+    {
+        $this->es = $es;
+
+        return $this;
+    }
+}
