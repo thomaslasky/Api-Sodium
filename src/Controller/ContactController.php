d12eb76171d1ba28469350e0fdd89d96d2ad198e
hugo-mtn7 <hugo.mtn7@gmail.com>
2018-11-26 15:03:40 +0100

    post reception

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ClientRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="contact", methods={"POST"})
      */
-    public function index()
+    public function receiveContact(Request $request)
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+
+
+        return $this->json(array('name' => $request->get('name')));
+        //return ['test' => [$request->get('name')]];
+        // return $this->render('contact/index.html.twig', [
+        //     'controller_name' => 'ContactController',
+        // ]);
     }
 }
